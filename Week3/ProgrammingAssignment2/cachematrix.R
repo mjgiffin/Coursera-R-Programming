3266655161036d5b07b49d578c729d58cc5033b0
Michael Giffin <giffin@amgen.com>
2021-03-18 16:25:58 -0700

    Added comments on assignment instructions

--- Week3/ProgrammingAssignment2/cachematrix.R
+++ Week3/ProgrammingAssignment2/cachematrix.R
@@ -2,6 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
+# Write the following functions:
+#   
+#   1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+#   2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
 
