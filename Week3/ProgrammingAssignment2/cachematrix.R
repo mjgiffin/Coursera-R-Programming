ee27f50f6443f996a6eb8da0e4937750a9793b2a
Michael Giffin <giffin@amgen.com>
2021-03-19 09:41:00 -0700

    first draft of cachematrix functions based on cachemean example

--- Week3/ProgrammingAssignment2/cachematrix.R
+++ Week3/ProgrammingAssignment2/cachematrix.R
@@ -3,12 +3,22 @@
 
 ## Write a short comment describing this function
 # Write the following functions:
-#   
+#
 #   1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 #   2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -16,4 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
 }
